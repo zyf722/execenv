6027fc598674806dcddf7c30e61d840aeaa18ee8
zyf722 <1124982772@qq.com>
2024-08-09 14:57:41 +0800

    chore: add clink completion

---
+++ scripts/execenv.lua
@@ -0,0 +1,10 @@
+clink.argmatcher("execenv")
+:addflags(
+    "--keep", "--no-keep",
+    "--verbose", "-v", "-vv",
+    "--version", "-V",
+    "--help", "-h",
+    "--env", "-e",
+    "--cwd", "-c",
+    "--env-file", "-f"
+)
